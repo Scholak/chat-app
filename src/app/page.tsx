9a6fdb19919da869ccce8bf4e59a1718f7aebd76
scholak <scholaksebahattin@gmail.com>
2023-07-10 21:06:13 +0300

    home page redirect issue fixed & mobile screen height issue solved (hopefully)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,11 @@
-import Profile from '@/components/Profile'
-import AddFriendForm from '@/components/friend/AddFriendForm'
-import FriendList from '@/components/friend/FriendList'
-import FriendRequest from '@/components/friend/FriendRequest'
 import React from 'react'
+import { redirect } from 'next/navigation'
 
 const Home = () => {
+	redirect('/chat')
   return (
 		<div>
-			<Profile />
-			<AddFriendForm />
-			<FriendRequest />
-			<FriendList />
+			Home
 		</div>
 	)
 }
